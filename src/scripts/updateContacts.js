0196cf775a6c81c4b65003e1119e004f345924d7
Kosianenko Mykola <112539586+KosianenkoMykola@users.noreply.github.com>
2024-08-31 19:29:37 +0300

    Initial commit

---
+++ src/scripts/updateContacts.js
@@ -0,0 +1,6 @@
+import * as fs from "node:fs/promises";
+import { PATH_DB } from "../constants/contacts.js";
+
+const updateContacts = contact => fs.writeFile(PATH_DB, JSON.stringify(contact, null, 2));
+
+export default updateContacts;
